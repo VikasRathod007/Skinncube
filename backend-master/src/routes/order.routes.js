18befa0cfb983e395d2fbae5d1ee87af4cc2b7f7
Vikash Rathod <vikeyrathod007@gmail.com>
2025-08-21 02:50:57 +0530

    refactor: remove duplicate backend-master folder to reduce project size

--- backend-master/src/routes/order.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { verifyJWT, isSA } from "../middlewares/auth.middleware.js";
-import { orderInitiate } from "../controllers/order.controller.js";
-
-const router = Router()
-
-router.route("/add-medicine").post(verifyJWT,orderInitiate);
-
-export default router
